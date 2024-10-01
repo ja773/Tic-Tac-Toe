0a1359aa0e1bef918ba8fd67ef35c12608dd39e2
ja773 <ja773@cam.ac.uk>
2024-10-01 23:01:39 +0100

    Defined render function to display board

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 
 
 def new_board():
+    ''' Function to initialise an empty board as a 2D list'''
     return [
         [None, None, None],
         [None, None, None],
         [None, None, None]
     ]
 
-print(new_board())
+
+def render(board):
+    ''' Function to print a board in a neat readable format'''
+    print('   0 1 2')
+    print(' '*3 + '-'*5)
+    for i in range(3):
+        row = str(i) + '| '
+        for j in board[i]:
+            if j == None:
+                row += ' '
+            else:
+                row += j
+            row += ' '
+        print(row)
+    print(' '*3 + '-'*5)
