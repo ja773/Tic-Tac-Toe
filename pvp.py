4e29e7e1c8e065c38d4dddf35136cd60996d2f08
ja773 <ja773@cam.ac.uk>
2024-10-02 01:52:45 +0100

    Added game ending due to wins to pvp.py

--- pvp.py
+++ pvp.py
@@ -1,10 +1,10 @@
-from main import new_board, make_move, is_move_valid, get_move, render
+from main import new_board, make_move, is_move_valid, get_move, render, get_winner
 
 # Starting the game with an empty board
 board = new_board()
 player1 = True          #to keep track of player
 
-while True:
+while not get_winner(board):
     render(board)
     if player1:
         print('Make a move for X')
@@ -19,5 +19,6 @@ while True:
         board = make_move(board, move_coords, 'O')
     player1 = not player1
 
-
+render(board)
+print('Winner is ',get_winner(board))
 
