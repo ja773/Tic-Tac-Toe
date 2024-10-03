0318c6b0043767615597f9df22b2f560886b66ca
ja773 <ja773@cam.ac.uk>
2024-10-03 01:31:26 +0100

    Initialised winning_and_losing_moves_ai.py; updated the AI to play winning and block losing moves where possible

---
+++ winning_and_losing_moves_ai.py
@@ -0,0 +1,26 @@
+from functions import new_board, make_move, is_move_valid_ai, render, get_winner, find_winning_and_losing_moves_ai
+
+# Starting the game with an empty board
+board = new_board()
+player1 = True   # to keep track of player
+
+moves = 0   # to keep track of number of moves played (<9) 
+while not get_winner(board) and moves < 9:
+    render(board)
+    if player1:
+        player = 'X'
+    else:
+        player = 'O'
+    move_coords = find_winning_and_losing_moves_ai(board, player)
+    if player1:
+        board = make_move(board, move_coords, 'X')
+    else:
+        board = make_move(board, move_coords, 'O')
+    player1 = not player1
+    moves += 1
+
+render(board)
+if not get_winner(board):
+    print('This game was a draw')
+else:
+    print('Winner is ',get_winner(board))
