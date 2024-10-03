6dc9a3b915910ce5d5083015e86b80f54cebdd5d
ja773 <ja773@cam.ac.uk>
2024-10-03 02:30:22 +0100

    Defined play_game_no_display in game.py

--- game.py
+++ game.py
@@ -27,3 +27,32 @@ def play_game(p1,p2):
         print('This game was a draw')
     else:
         print('Winner is ',get_winner(board))
+
+def play_game_no_display(p1,p2):
+    ''' 
+    Takes 2 interface classes as parameters and plays each other in a game 
+    No printing output to be used for mass repetitions
+    '''
+    # Starting the game with an empty board
+    board = new_board()
+    player1 = True   # to keep track of player
+
+    moves = 0   # to keep track of number of moves played (<9) 
+    while not get_winner(board) and moves < 9:
+        #render(board)
+        if player1:
+            player = 'X'
+            move_coords = p1.make_move(board, player)
+        else:
+            player = 'O'
+            move_coords = p2.make_move(board, player)
+        if player1:
+            board = make_move(board, move_coords, 'X')
+        else:
+            board = make_move(board, move_coords, 'O')
+        player1 = not player1
+        moves += 1
+
+    winner = get_winner(board)
+
+    return winner
