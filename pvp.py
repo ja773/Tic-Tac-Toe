00855e488df475ecdeba32d4ee0b798055ff31ec
ja773 <ja773@cam.ac.uk>
2024-10-03 01:42:07 +0100

    Updated pvp.py with human_player interface

--- pvp.py
+++ pvp.py
@@ -1,4 +1,4 @@
-from functions import new_board, make_move, is_move_valid, get_move, render, get_winner
+from functions import new_board, make_move, is_move_valid, human_player, render, get_winner
 
 # Starting the game with an empty board
 board = new_board()
@@ -8,12 +8,11 @@ moves = 0   # to keep track of number of moves played (<9)
 while not get_winner(board) and moves < 9:
     render(board)
     if player1:
-        print('Make a move for X')
+        player = 'X'
     else:
-        print('Make a move for O')
-    move_coords = get_move()
-    while not is_move_valid(board,move_coords):
-        move_coords = get_move()
+        player = 'O'
+    print('Make a mover for ',player)
+    move_coords = human_player(board, player)
     if player1:
         board = make_move(board, move_coords, 'X')
     else:
