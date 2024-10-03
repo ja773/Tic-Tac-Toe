282f11cd119f4bfca1632048aa4b4b85921c28a3
ja773 <ja773@cam.ac.uk>
2024-10-03 03:11:29 +0100

    Updated repeat_statistics.py

--- repeat_statistics.py
+++ repeat_statistics.py
@@ -1,39 +1,12 @@
 from functions import new_board, make_move, render, get_winner
 from functions import human_player, random_ai, find_winning_moves_ai, find_winning_and_losing_moves_ai
 from game import play_game, play_game_no_display
+from tictactoe import TicTacToeAI, HumanPlayer, RandomAI, WinningMoveAI, WinningAndLosingMoveAI, classMap
 import sys
-
-# Base class for AI
-class TicTacToeAI:
-    def make_move(self, board, player):
-        raise NotImplementedError("This method should be overridden by subclasses")
-    
-class RandomAI(TicTacToeAI):
-    def make_move(self, board, player):
-        return random_ai(board,player)
-
-class WinningMoveAI(TicTacToeAI):
-    def make_move(self, board, player):
-        return find_winning_moves_ai(board,player)
-
-class WinningAndLosingMoveAI(TicTacToeAI):
-    def make_move(self, board, player):
-        return find_winning_and_losing_moves_ai(board,player)
-
-class HumanPlayer(TicTacToeAI):
-    def make_move(self, board, player):
-        return human_player(board,player)
-
-classMap = {
-    'random_ai' : RandomAI,
-    'find_winning_moves_ai' : WinningMoveAI,
-    'find_winning_and_losing_moves_ai' : WinningAndLosingMoveAI,
-    'human_player' : HumanPlayer
-}
 
 def repeat_stats():
     ''' Function to play 2 AIs multiple times and return result statistics'''
-    
+
     i1_name = input('Enter AI interface for player X: ')
 
     if i1_name not in classMap:
